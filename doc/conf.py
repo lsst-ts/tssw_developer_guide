bdae64c2e11cfb58b7f64c54b8d610ba6907f468
Eric Coughlin <ecoughlin@lsst.org>
2023-02-16 14:41:38 -0300

    Add DevelopPipeline documentation

--- doc/conf.py
+++ doc/conf.py
@@ -1,11 +1,1 @@
-from documenteer.conf.pipelinespkg import *
-
-project = "TSSW Developer Guide"
-html_theme_options["logotext"] = project
-html_title = project
-html_short_title = project
-
-extensions.append('sphinx.ext.autosectionlabel')
-autosectionlabel_prefix_document=True
-
-intersphinx_mapping['dm_developer_guide'] = ('https://developer.lsst.io', None)
+from documenteer.conf.guide import *
